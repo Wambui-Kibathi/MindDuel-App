23e7f7fa6a0bb5be0e60dea334ea59655c261932
Wambui Kibathi <zeldabobo02@gmail.com>
2025-07-27 16:02:32 +0300

    WIP: General structure of TopicsCard.jsx

---
+++ src/components/Home/TopicsCard.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+import { getSlugFromTopicName } from '../../utils/topics';
+import '../../App.css';
+
+export function TopicsCard() {
+    return (
+        <>
+        </>
+    );
+}
