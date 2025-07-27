5e2fa4e3532c3c4c273d4139092d74ce69743c47
MosingiA <ondicho33@gmail.com>
2025-07-27 22:59:08 +0300

    error deriving

---
+++ src/components/Layout/ErrorBoundary.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import '../../App.css';
+
+class ErrorBoundary extends Component {
+  state = { hasError: false, error: null };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Error Boundary caught:", error, errorInfo);
+  }
+}
+
+export default ErrorBoundary;
