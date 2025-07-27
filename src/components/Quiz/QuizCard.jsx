6f6dac2b985deb8647d33e6e220385ee70abcb1c
Wambui Kibathi <zeldabobo02@gmail.com>
2025-07-27 18:32:58 +0300

    WIP: General structure of the QuizCard.jsx file

---
+++ src/components/Quiz/QuizCard.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
+import { getTopicNameFromSlug } from '../../utils/topics';
+import Timer from './Timer';
+import { FaFlagCheckered, FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import '../../App.css';
+
+export function QuizCard() {
+    return (
+        <div>
+        </div>
+    );
+}
