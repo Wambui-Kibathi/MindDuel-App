1f1c316fd16cad1690bd54dd5e417b3bec0f0e30
Wambui Kibathi <zeldabobo02@gmail.com>
2025-07-28 12:11:30 +0300

    Fix server.js module error

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-// server.js
-const jsonServer = require('json-server');
+import jsonServer from 'json-server';
+import cors from 'cors';
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const cors = require('cors');
 
-// Enable CORS so your frontend on Vercel can fetch from Render backend
 server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-// Use the PORT provided by Render, or default to 3000 locally
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
